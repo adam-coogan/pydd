ae60c2a10dde98363152f8b9cc02a2c745ec0b55
Adam Coogan <dr.adam.coogan@gmail.com>
2021-03-26 13:57:19 +0100

    add basic tests

--- test_binary.py
+++ test_binary.py
@@ -4,7 +4,12 @@ from analysis_jax import calculate_SNR, loglikelihood
 from binary_jax import DynamicDress, get_f_isco, get_m_1
 
 
-def test_SNR_loglikelihood():
+def test_SNR_loglikelihood(verbose=False):
+    f_c_s = get_f_isco(get_m_1(jnp.array(3.151009407916561e31), jnp.array(0.001)))
+    f_c_h = get_f_isco(
+        get_m_1(jnp.array(3.151580260164573e31), jnp.array(0.0005415094825728555))
+    )
+    f_h = jnp.maximum(f_c_s, f_c_h)
     dd_s = DynamicDress(
         jnp.array(2.3333333333333335),
         jnp.array(0.00018806659428775589),
@@ -13,8 +18,8 @@ def test_SNR_loglikelihood():
         jnp.array(0.0),
         jnp.array(0.0),
         jnp.array(-56.3888135025341),
+        f_c_s,
     )
-    f_c = get_f_isco(get_m_1(dd_s.M_chirp, dd_s.q))
     f_l = 0.022621092492458004  # Hz
 
     dd_h = DynamicDress(
@@ -25,20 +30,18 @@ def test_SNR_loglikelihood():
         jnp.array(dd_s.Phi_c),
         jnp.array(-227.74995698),
         jnp.array(dd_s.dL_iota),
+        f_c_h,
     )
-    f_c_h = get_f_isco(get_m_1(dd_h.M_chirp, dd_h.q))
 
-    print("SNR(s) = ", calculate_SNR(dd_s, f_c, "d", f_l, f_c, 3000))
-    print("SNR(h) = ", calculate_SNR(dd_h, f_c_h, "d", f_l, f_c_h, 3000))
-    print(
-        "log L(s|s) = ",
-        loglikelihood(dd_s, f_c, "d", dd_s, f_c, "d", f_l, f_c),
-    )
-    print(
-        "log L(h|h) = ",
-        loglikelihood(dd_h, f_c_h, "d", dd_h, f_c_h, "d", f_l, f_c_h),
-    )
-    print(
-        "log L(h|s) = ",
-        loglikelihood(dd_h, f_c_h, "d", dd_s, f_c, "d", f_l, f_c),
-    )
+    snr_s = calculate_SNR(dd_s, f_l, f_h, 3000).block_until_ready()
+    snr_h = calculate_SNR(dd_h, f_l, f_h, 3000).block_until_ready()
+    ll_ss = loglikelihood(dd_s, dd_s, f_l, f_h, 3000, 3000).block_until_ready()
+    ll_hh = loglikelihood(dd_h, dd_h, f_l, f_h, 3000, 3000).block_until_ready()
+    ll_hs = loglikelihood(dd_h, dd_s, f_l, f_h, 3000, 3000).block_until_ready()
+
+    if verbose:
+        print("SNR(s) = ", snr_s)
+        print("SNR(h) = ", snr_h)
+        print("log L(s|s) = ", ll_ss)
+        print("log L(h|h) = ", ll_hh)
+        print("log L(h|s) = ", ll_hs)
