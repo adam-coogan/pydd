d2b2ebb4d1e2bdd5a439cc0707613391918112a0
Adam Coogan <dr.adam.coogan@gmail.com>
2021-07-07 15:54:18 -0400

    update hypgeom test

--- test/test_hypgeom_interp.py
+++ test/test_hypgeom_interp.py
@@ -1,29 +1,32 @@
-from jax import jit
+import jax
+from jax import random
 import jax.numpy as jnp
-import numpy.random as np_random
 
-from pydd.binary import get_hypgeom_interps, hypgeom_scipy
-
-
-"""
-TODO: revise. Wrong parameter ranges.
-"""
+from pydd.binary import hypgeom_jax, hypgeom_scipy
 
 
-def test_hypgeom_interps(n_samples=10000, far_frac=0.005):
-    interp_pos, interp_neg = get_hypgeom_interps(5000, 4950)
-    interp_pos_jit, interp_neg_jit = jit(interp_pos), jit(interp_neg)
+def test_hypgeom_interps():
+    """
+    Make sure the interpolated hypergeometric function agrees to 0.001-0.01%
+    with scipy.
+    """
+    n_samples = 1000
+    key = random.PRNGKey(1234)
 
-    bs = jnp.array(np_random.uniform(1.55, 1.99, size=n_samples))
-    log10_abs_zs = jnp.array(np_random.uniform(-4, 4, size=n_samples))
-    zs = -(10 ** log10_abs_zs)
+    # b > 0
+    key, b_key, z_key = random.split(key, 3)
+    bs = random.uniform(b_key, (n_samples,), minval=0.5, maxval=1.99)
+    log10_abs_zs = random.uniform(z_key, (n_samples,), minval=-4.0, maxval=4.0)
+    v_scipy = hypgeom_scipy(bs, -(10 ** log10_abs_zs))
+    v_jax = jax.lax.map(lambda args: hypgeom_jax(*args), (bs, -(10 ** log10_abs_zs)))
+    assert jnp.allclose(v_scipy, v_jax, rtol=1e-5)
 
-    vals_pos = interp_pos_jit(bs, zs)
-    vals_neg = interp_neg_jit(-bs, zs)
-
-    assert (
-        ~jnp.isclose(hypgeom_scipy(bs, zs), vals_pos, atol=0)
-    ).sum() < far_frac * n_samples
-    assert (
-        ~jnp.isclose(hypgeom_scipy(-bs, zs), vals_neg, atol=0)
-    ).sum() < far_frac * n_samples
+    # b < 0
+    # The b values here are restricted, but cover the range relevant for dark
+    # dress calculations
+    key, b_key, z_key = random.split(key, 3)
+    bs = random.uniform(b_key, (1000,), minval=-1.95, maxval=-1.1)
+    log10_abs_zs = random.uniform(z_key, (1000,), minval=-4.0, maxval=4.0)
+    v_scipy = hypgeom_scipy(bs, -(10 ** log10_abs_zs))
+    v_jax = jax.lax.map(lambda args: hypgeom_jax(*args), (bs, -(10 ** log10_abs_zs)))
+    assert jnp.allclose(v_scipy, v_jax, rtol=1e-4)
