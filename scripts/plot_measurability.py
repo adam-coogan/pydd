50d258794772151526ed825c5c518115edcb1b30
Adam Coogan <dr.adam.coogan@gmail.com>
2021-08-05 18:17:08 +0200

    add final data files and plots; clean up scripts

---
+++ scripts/plot_measurability.py
@@ -0,0 +1,123 @@
+from math import log10
+import os
+import pickle
+
+import dynesty
+from dynesty import plotting as dyplot
+import matplotlib.pyplot as plt
+import numpy as np
+
+from pydd.binary import MSUN, get_M_chirp
+from utils import (
+    GAMMA_S_ASTRO,
+    GAMMA_S_PBH,
+    M_1_BM,
+    M_2_BM,
+    RHO_6_ASTRO,
+    RHO_6_PBH,
+    rho_6_to_rho6T,
+)
+
+"""
+Plot nested sampling posteriors for the astrophysical and PBH benchmarks.
+
+Requires `ns/rho_6T=0.5448_gamma_s=2.33333.pkl` and
+`ns/rho_6T=0.5345_gamma_s=2.25.pkl`, which are produced by
+`job_measurability.sh`.
+
+Produces `figures/ns-astro.pdf` and `figures/ns-pbh.pdf`
+"""
+
+labels = (
+    r"$\gamma_s$",
+    r"$\rho_6$ [$10^{16}$ $\mathrm{M}_\odot \, \mathrm{pc}^{-3}$]",
+    r"$\mathcal{M}$ [M$_\odot$]",
+    r"$\log_{10} q$",
+)
+quantiles_2d = [1 - np.exp(-(x ** 2) / 2) for x in [1, 2, 3]]
+smooth = 0.01
+
+
+def get_base_path(rho_6, gamma_s):
+    return f"rho_6T={rho_6_to_rho6T(rho_6):g}_gamma_s={gamma_s:g}"
+
+
+def plot_astro():
+    base_path = get_base_path(RHO_6_ASTRO, GAMMA_S_ASTRO)
+    with open(os.path.join("ns", f"{base_path}.pkl"), "rb") as infile:
+        results = pickle.load(infile)
+
+    cfig, axes = dyplot.cornerplot(
+        results,
+        labels=labels,
+        quantiles_2d=quantiles_2d,
+        smooth=0.015,
+        show_titles=True,
+        title_kwargs={"fontsize": 12},
+        span=[(2.25, 2.42), (0.42, 0.72), (19.386, 19.394), (-3.3, -2.5)],
+        max_n_ticks=6,
+        truths=(
+            GAMMA_S_ASTRO,
+            rho_6_to_rho6T(RHO_6_ASTRO),
+            get_M_chirp(M_1_BM, M_2_BM) / MSUN,
+            log10(M_2_BM / M_1_BM),
+        ),
+    )
+    # Deal with chirp mass manually
+    axes[2, 0].set_yticklabels([f"{t:.4f}" for t in axes[2, 0].get_yticks()])
+    Mc_quantiles = dynesty.utils.quantile(results.samples[:, 2], [0.025, 0.5, 0.975])
+    axes[2, 2].set_title(
+        labels[2]
+        + r" = ${%.3f}_{-%.3f}^{+%.3f}$"
+        % (
+            Mc_quantiles[1],
+            Mc_quantiles[1] - Mc_quantiles[0],
+            Mc_quantiles[2] - Mc_quantiles[1],
+        ),
+        fontsize=12,
+    )
+    cfig.tight_layout(pad=0.2)
+    cfig.savefig(os.path.join("figures", f"ns-astro.pdf"))
+
+
+def plot_pbh():
+    base_path = get_base_path(RHO_6_PBH, GAMMA_S_PBH)
+    with open(os.path.join("ns", f"{base_path}.pkl"), "rb") as infile:
+        results = pickle.load(infile)
+
+    cfig, axes = dyplot.cornerplot(
+        results,
+        labels=labels,
+        quantiles_2d=quantiles_2d,
+        smooth=0.015,
+        show_titles=True,
+        title_kwargs={"fontsize": 12},
+        span=[(2.25, 2.325), (0.4, 0.75), (19.3875, 19.3895), (-3.0, -2.5)],
+        max_n_ticks=6,
+        truths=(
+            GAMMA_S_PBH,
+            rho_6_to_rho6T(RHO_6_PBH),
+            get_M_chirp(M_1_BM, M_2_BM) / MSUN,
+            log10(M_2_BM / M_1_BM),
+        ),
+    )
+    # Deal with chirp mass manually
+    axes[2, 0].set_yticklabels([f"{t:.4f}" for t in axes[2, 0].get_yticks()])
+    Mc_quantiles = dynesty.utils.quantile(results.samples[:, 2], [0.025, 0.5, 0.975])
+    axes[2, 2].set_title(
+        labels[2]
+        + r" = ${%.3f}_{-%.3f}^{+%.3f}$"
+        % (
+            Mc_quantiles[1],
+            Mc_quantiles[1] - Mc_quantiles[0],
+            Mc_quantiles[2] - Mc_quantiles[1],
+        ),
+        fontsize=12,
+    )
+    cfig.tight_layout(pad=0.2)
+    cfig.savefig(os.path.join("figures", f"ns-pbh.pdf"))
+
+
+if __name__ == "__main__":
+    plot_astro()
+    plot_pbh()
---
+++ scripts/plot_snr.py
@@ -0,0 +1,72 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import root_scalar
+from tqdm.auto import tqdm
+
+import jax.numpy as jnp
+
+from pydd.analysis import calculate_SNR
+from pydd.binary import *
+
+"""
+Plots SNRs for GR-in-vacuum binaries as a function of chirp mass and luminosity
+distance.
+
+Produces `figures/snrs.pdf`.
+"""
+
+if __name__ == "__main__":
+    t_obs_lisa = 5 * YR
+    f_c = 1e2  # get_f_isco(1e3 * MSUN)
+    M_chirp_min = 10 * MSUN  # get_M_chirp(1e3 * MSUN, 1 * MSUN)
+    M_chirp_max = 2000 * MSUN  # get_M_chirp(1e5 * MSUN, 1e2 * MSUN)
+    dL_min = 1e6 * PC
+    dL_max = 10e9 * PC
+
+    M_chirps = jnp.geomspace(M_chirp_min, M_chirp_max, 40)
+    dLs = jnp.geomspace(dL_min, dL_max, 35)
+
+    snrs = np.zeros([len(M_chirps), len(dLs)])
+    f_ls = np.zeros([len(M_chirps), len(dLs)])
+
+    for i, M_chirp in enumerate(tqdm(M_chirps)):
+        for j, dL in enumerate(dLs):
+            dd_v = VacuumBinary(M_chirp, 0.0, 0.0, dL, f_c)
+            f_l = root_scalar(
+                lambda f: t_to_c(f, dd_v) - t_obs_lisa,
+                bracket=(1e-3, 1e-1),
+                rtol=1e-15,
+                xtol=1e-100,
+            ).root
+            f_ls[i, j] = f_l
+            snrs[i, j] = calculate_SNR(dd_v, f_l, f_c, 3000)
+
+    plt.figure(figsize=(4, 3.5))
+
+    plt.axvline(get_M_chirp(1e3, 1.4), color="r", linestyle="--")
+    plt.axhline(76, color="r", linestyle="--")
+    plt.xscale("log")
+    plt.yscale("log")
+    cs = plt.contour(
+        M_chirps / MSUN,
+        dLs / (1e6 * PC),
+        jnp.log10(snrs.T),
+        levels=jnp.linspace(-2, 6, 9).round(),
+        alpha=0.8,
+    )
+    plt.clabel(cs, inline=True, fontsize=10, fmt=r"$10^{%i}$")
+
+    cs = plt.contour(
+        M_chirps / MSUN,
+        dLs / (1e6 * PC),
+        snrs.T,
+        levels=[15],
+        colors=["r"],
+    )
+    plt.clabel(cs, inline=True, fontsize=10, fmt=r"%g")
+
+    plt.xlabel(r"$\mathcal{M}$ [M$_\odot$]")
+    plt.ylabel(r"$d_L$ [Mpc]")
+    plt.tight_layout()
+
+    plt.savefig("figures/snrs.pdf")
